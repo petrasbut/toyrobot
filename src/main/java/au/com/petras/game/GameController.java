a206beb04274866ad9c109e4d795634c51faedce
petras <petrasbut@gmaill.com>
2018-11-14 21:43:17 +1100

    everything seems to be working.

--- src/main/java/au/com/petras/game/GameController.java
+++ src/main/java/au/com/petras/game/GameController.java
@@ -1,12 +1,12 @@
 package au.com.petras.game;
 
 import au.com.petras.game.exception.GameCmdException;
+import au.com.petras.game.parser.GameCmdParser;
 import au.com.petras.robot.Direction;
 import au.com.petras.robot.Position;
 import au.com.petras.robot.Robot;
 import au.com.petras.robot.RobotInterface;
 import au.com.petras.tableTop.TableTop;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -53,7 +53,7 @@ public class GameController implements RobotInterface {
 
     public void action(String inputString) throws GameCmdException {
 
-        GameCmd cmd = parseCommand(inputString);
+        GameCmd cmd = GameCmdParser.parseCommand(inputString);
 
         switch (cmd.input) {
             case PLACE:
@@ -76,37 +76,5 @@ public class GameController implements RobotInterface {
         return;
     }
 
-    private GameCmd parseCommand(String inputString) throws GameCmdException {
 
-        String[] rawCmd = StringUtils.split(inputString);
-
-        // cmd identification
-        GameInput input = null;
-        try {
-            input = GameInput.valueOf(rawCmd[0]);
-        } catch (IllegalArgumentException e) {
-            throw new GameCmdException(e, rawCmd);
-        }
-
-        if (input.equals(GameInput.PLACE)) {
-            return parsePlaceCmd(StringUtils.split(rawCmd[1], ','));
-        } else {
-            return new GameCmd(input, null, null);
-        }
-    }
-
-    private GameCmd parsePlaceCmd(String[] rawCmd) throws GameCmdException {
-
-        try {
-            int[] xy = new int[2];
-            xy[0] = Integer.valueOf(rawCmd[0]);
-            xy[1] = Integer.valueOf(rawCmd[1]);
-            Direction direction = Direction.valueOf(rawCmd[2].toUpperCase());
-
-            return new GameCmd(GameInput.PLACE, xy, direction);
-
-        } catch (RuntimeException e) {
-            throw new GameCmdException(e, rawCmd);
-        }
-    }
 }
---
+++ src/main/java/au/com/petras/game/parser/GameCmdParser.java
@@ -0,0 +1,44 @@
+package au.com.petras.game.parser;
+
+import au.com.petras.game.GameCmd;
+import au.com.petras.game.GameInput;
+import au.com.petras.game.exception.GameCmdException;
+import au.com.petras.robot.Direction;
+import org.apache.commons.lang3.StringUtils;
+
+public class GameCmdParser {
+
+    public static GameCmd parseCommand(String inputString) throws GameCmdException {
+
+        String[] rawCmd = StringUtils.split(inputString);
+
+        // cmd identification
+        GameInput input = null;
+        try {
+            input = GameInput.valueOf(rawCmd[0]);
+        } catch (IllegalArgumentException e) {
+            throw new GameCmdException(e, rawCmd);
+        }
+
+        if (input.equals(GameInput.PLACE)) {
+            return parsePlaceCmd(StringUtils.split(rawCmd[1], ','));
+        } else {
+            return new GameCmd(input, null, null);
+        }
+    }
+
+    public static GameCmd parsePlaceCmd(String[] rawCmd) throws GameCmdException {
+
+        try {
+            int[] xy = new int[2];
+            xy[0] = Integer.valueOf(rawCmd[0]);
+            xy[1] = Integer.valueOf(rawCmd[1]);
+            Direction direction = Direction.valueOf(rawCmd[2].toUpperCase());
+
+            return new GameCmd(GameInput.PLACE, xy, direction);
+
+        } catch (RuntimeException e) {
+            throw new GameCmdException(e, rawCmd);
+        }
+    }
+}
